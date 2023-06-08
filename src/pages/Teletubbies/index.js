c7f60c3cc677a5574ab358241e7d97cb3f5555d5
michioyamada <skygentleman626@gmail.com>
2023-06-08 17:58:55 +0900

    got and showed 20 json data

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -1,18 +1,69 @@
 import { useEffect, useState } from "react";
+import { Container, Grid, Box, Typography, Button } from "@mui/material";
 
 const Teletubbies = () => {
   const [teletubbies, setTeletubbies] = useState([]);
+  const [visibleTeletubbies, setVisibleTeletubbies] = useState([]);
   useEffect(() => {
     fetch("/teletubbies.json")
       .then((response) => response.json())
       .then((data) => {
         setTeletubbies(data);
+        setVisibleTeletubbies(data.slice(0, 20));
       });
   }, []);
-  console.log(teletubbies);
   return (
     <>
-      <h1>Hellow World!</h1>
+      <Container maxWidth="lg">
+        <Typography variant="h1" sx={{ mt: 4 }}>
+          Teletubbies
+        </Typography>
+        <Grid container spacing={2}>
+          {visibleTeletubbies.map((teletubby, key) => {
+            return (
+              <Grid
+                container
+                direction={key % 2 ? "row-reverse" : "row"}
+                key={key}
+                alignItems="center"
+                sx={{ marginTop: 1, border: 1 }}
+              >
+                <Grid item md={4} lg={2} sm={6} xs={12}>
+                  <Box sx={{ maxWidth: "100%" }}>
+                    <img
+                      src={teletubby.image_url}
+                      alt={teletubby.name}
+                      style={{ width: "100%" }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item md={8} lg={10} sm={6} xs={12}>
+                  <Box>
+                    <Typography variant="h3">{teletubby.name}</Typography>
+                    <Typography variant="body1" sx={{ mt: 2 }}>
+                      {teletubby.description}
+                    </Typography>
+                    <Box sx={{ mt: 2 }}>
+                      {teletubby.traits.map((item, key) => {
+                        return (
+                          <Button
+                            variant="outlined"
+                            color="primary"
+                            key={key}
+                            sx={{ mr: 2, mb: 1 }}
+                          >
+                            {item}
+                          </Button>
+                        );
+                      })}
+                    </Box>
+                  </Box>
+                </Grid>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </>
   );
 };
