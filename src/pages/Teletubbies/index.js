2a05c70f78a1007d063bdc91da44d8db76e68c5f
michioyamada <skygentleman626@gmail.com>
2023-06-08 11:00:54 +0900

    import json data using fetch

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 const Teletubbies = () => {
+  const [teletubbies, setTeletubbies] = useState([]);
+  useEffect(() => {
+    fetch("/teletubbies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTeletubbies(data);
+      });
+  }, []);
+  console.log(teletubbies);
   return (
     <>
       <h1>Hellow World!</h1>
