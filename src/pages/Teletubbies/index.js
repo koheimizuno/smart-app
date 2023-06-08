e859ad8df718a48edb68e4a72585cf4f6b7316fa
michioyamada <skygentleman626@gmail.com>
2023-06-09 03:54:48 +0900

    removed eslint warning

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -38,6 +38,7 @@ const Teletubbies = () => {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
+    // eslint-disable-next-line
   }, [visibleTeletubbies]);
 
   const handleInputChange = (event) => {
@@ -52,66 +53,64 @@ const Teletubbies = () => {
   }, [searchTerm, teletubbies]);
 
   return (
-    <>
-      <Container maxWidth="lg">
-        <Typography variant="h1" sx={{ mt: 4 }}>
-          Teletubbies
-        </Typography>
-        <TextField
-          label="Search by name"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          value={searchTerm}
-          onChange={handleInputChange}
-        />
-        <Grid container>
-          {visibleTeletubbies.map((teletubby, key) => {
-            return (
-              <Grid
-                container
-                direction={key % 2 ? "row-reverse" : "row"}
-                key={key}
-                alignItems="center"
-                sx={{ marginTop: 1, border: 1 }}
-              >
-                <Grid item md={4} lg={2} sm={6} xs={12}>
-                  <Box sx={{ maxWidth: "100%" }}>
-                    <img
-                      src={teletubby.image_url}
-                      alt={teletubby.name}
-                      style={{ width: "100%" }}
-                    />
-                  </Box>
-                </Grid>
-                <Grid item md={8} lg={10} sm={6} xs={12}>
-                  <Box>
-                    <Typography variant="h3">{teletubby.name}</Typography>
-                    <Typography variant="body1" sx={{ mt: 2 }}>
-                      {teletubby.description}
-                    </Typography>
-                    <Box sx={{ mt: 2 }}>
-                      {teletubby.traits.map((item, key) => {
-                        return (
-                          <Button
-                            variant="outlined"
-                            color="primary"
-                            key={key}
-                            sx={{ mr: 2, mb: 1 }}
-                          >
-                            {item}
-                          </Button>
-                        );
-                      })}
-                    </Box>
-                  </Box>
-                </Grid>
+    <Container maxWidth="lg">
+      <Typography variant="h1" sx={{ mt: 4 }}>
+        Teletubbies
+      </Typography>
+      <TextField
+        label="Search by name"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <Grid container>
+        {visibleTeletubbies.map((teletubby, key) => {
+          return (
+            <Grid
+              container
+              direction={key % 2 ? "row-reverse" : "row"}
+              key={key}
+              alignItems="center"
+              sx={{ marginTop: 1, border: 1 }}
+            >
+              <Grid item md={4} lg={2} sm={6} xs={12}>
+                <Box sx={{ maxWidth: "100%" }}>
+                  <img
+                    src={teletubby.image_url}
+                    alt={teletubby.name}
+                    style={{ width: "100%" }}
+                  />
+                </Box>
               </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
-    </>
+              <Grid item md={8} lg={10} sm={6} xs={12}>
+                <Box>
+                  <Typography variant="h3">{teletubby.name}</Typography>
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    {teletubby.description}
+                  </Typography>
+                  <Box sx={{ mt: 2 }}>
+                    {teletubby.traits.map((item, key) => {
+                      return (
+                        <Button
+                          variant="outlined"
+                          color="primary"
+                          key={key}
+                          sx={{ mr: 2, mb: 1 }}
+                        >
+                          {item}
+                        </Button>
+                      );
+                    })}
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 
