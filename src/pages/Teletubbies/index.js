87bbc7137aa143b68015bd470893aaff9e11f6b2
michioyamada <skygentleman626@gmail.com>
2023-06-08 20:43:56 +0900

    added feature to list more when user scrolls down the page

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -3,20 +3,38 @@ import { Container, Grid, Box, Typography, Button } from "@mui/material";
 
 const Teletubbies = () => {
   const [teletubbies, setTeletubbies] = useState([]);
-  useEffect(() => { 
+  const [visibleTeletubbies, setVisibleTeletubbies] = useState([]);
+  useEffect(() => {
     fetch("/teletubbies.json")
       .then((response) => response.json())
       .then((data) => {
-        setTeletubbies(data.slice(0, 20));
+        setTeletubbies(data);
+        setVisibleTeletubbies(data.slice(0, 20));
       });
   }, []);
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      const newVisibleTeletubbies = [
+        ...visibleTeletubbies,
+        ...teletubbies.slice(
+          visibleTeletubbies.length,
+          visibleTeletubbies.length + 20
+        ),
+      ];
+      setVisibleTeletubbies(newVisibleTeletubbies);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [visibleTeletubbies]);
   return (
     <Container maxWidth="lg">
       <Typography variant="h1" sx={{ mt: 4 }}>
         Teletubbies
       </Typography>
       <Grid container spacing={2}>
-        {teletubbies.map((teletubby, key) => {
+        {visibleTeletubbies.map((teletubby, key) => {
           return (
             <Grid
               container
