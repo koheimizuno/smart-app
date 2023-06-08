d91f3d43b7f53cf58e24e255df0e19c0e85ff94e
michioyamada <skygentleman626@gmail.com>
2023-06-08 19:10:50 +0900

    removed "<></>"

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -29,58 +29,56 @@ const Teletubbies = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [visibleTeletubbies]);
   return (
-    <>
-      <Container maxWidth="lg">
-        <Typography variant="h1" sx={{ mt: 4 }}>
-          Teletubbies
-        </Typography>
-        <Grid container spacing={2}>
-          {visibleTeletubbies.map((teletubby, key) => {
-            return (
-              <Grid
-                container
-                direction={key % 2 ? "row-reverse" : "row"}
-                key={key}
-                alignItems="center"
-                sx={{ marginTop: 1, border: 1 }}
-              >
-                <Grid item md={4} lg={2} sm={6} xs={12}>
-                  <Box sx={{ maxWidth: "100%" }}>
-                    <img
-                      src={teletubby.image_url}
-                      alt={teletubby.name}
-                      style={{ width: "100%" }}
-                    />
-                  </Box>
-                </Grid>
-                <Grid item md={8} lg={10} sm={6} xs={12}>
-                  <Box>
-                    <Typography variant="h3">{teletubby.name}</Typography>
-                    <Typography variant="body1" sx={{ mt: 2 }}>
-                      {teletubby.description}
-                    </Typography>
-                    <Box sx={{ mt: 2 }}>
-                      {teletubby.traits.map((item, key) => {
-                        return (
-                          <Button
-                            variant="outlined"
-                            color="primary"
-                            key={key}
-                            sx={{ mr: 2, mb: 1 }}
-                          >
-                            {item}
-                          </Button>
-                        );
-                      })}
-                    </Box>
-                  </Box>
-                </Grid>
+    <Container maxWidth="lg">
+      <Typography variant="h1" sx={{ mt: 4 }}>
+        Teletubbies
+      </Typography>
+      <Grid container spacing={2}>
+        {visibleTeletubbies.map((teletubby, key) => {
+          return (
+            <Grid
+              container
+              direction={key % 2 ? "row-reverse" : "row"}
+              key={key}
+              alignItems="center"
+              sx={{ marginTop: 1, border: 1 }}
+            >
+              <Grid item md={4} lg={2} sm={6} xs={12}>
+                <Box sx={{ maxWidth: "100%" }}>
+                  <img
+                    src={teletubby.image_url}
+                    alt={teletubby.name}
+                    style={{ width: "100%" }}
+                  />
+                </Box>
               </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
-    </>
+              <Grid item md={8} lg={10} sm={6} xs={12}>
+                <Box>
+                  <Typography variant="h3">{teletubby.name}</Typography>
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    {teletubby.description}
+                  </Typography>
+                  <Box sx={{ mt: 2 }}>
+                    {teletubby.traits.map((item, key) => {
+                      return (
+                        <Button
+                          variant="outlined"
+                          color="primary"
+                          key={key}
+                          sx={{ mr: 2, mb: 1 }}
+                        >
+                          {item}
+                        </Button>
+                      );
+                    })}
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 export default Teletubbies;
