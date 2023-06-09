8e6ea0dbcbd7102b11627f0c0714f5a28cb3d893
michioyamada <skygentleman626@gmail.com>
2023-06-09 14:44:18 +0900

    implemented fixed header

--- src/pages/NFTS/NFTs.component.js
+++ src/pages/NFTS/NFTs.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
@@ -7,39 +7,72 @@ import Box from "@mui/material/Box";
 
 const Nfts_PAGE = () => {
   const [listing, setListing] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredListing, setFilteredListing] = useState([]);
 
-  const loadListing = async () => {
+  const loadListing = useCallback(async () => {
     const response = await fetch(
-      `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=0`
+      `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${offset}`
     );
     const data = await response.json();
     setListing([...listing, ...data.results]);
+    setFilteredListing([...listing, ...data.results]);
+    setOffset(offset + 20);
+  }, [offset, listing]);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
   };
 
   useEffect(() => {
     loadListing();
-    // eslint-disable-next-line
-  }, []);
+  }, [loadListing]);
+
+  useEffect(() => {
+    setFilteredListing(
+      listing.filter((listing) =>
+        listing.collectionName.toLowerCase().includes(searchTerm)
+      )
+    );
+  }, [listing, searchTerm]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 1000
+      ) {
+        loadListing();
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [loadListing]);
 
   return (
     <Container maxWidth="xl">
-      <Typography variant="h2">NFT Marketplace</Typography>
+      <Typography variant="h2" sx={{ m: "60px" }}>
+        NFT Marketplace
+      </Typography>
       <TextField
         id="outlined-basic"
         label="Outlined"
         variant="outlined"
         type="text"
         placeholder="Search by name"
-        value={""}
-        onChange={""}
-        sx={{ m: "30px" }}
+        value={searchTerm}
+        onChange={handleSearch}
+        sx={{ m: "10px" }}
       />
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        {listing.map((listing) => (
+        {filteredListing.map((listing) => (
           <Grid item className="card" key={listing.id} xs={3}>
             <img
               style={{ width: "100%", height: "70%" }}
